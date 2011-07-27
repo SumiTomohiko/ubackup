5b591486bafb72b4f93f2ecb7fd6d832e4f4cf77
SumiTomohiko <SumiTomohiko@neko-daisuki.ddo.jp>
2011-07-27 21:39:07 +0900

    Changed ubtc.c to use Client structure.

--- ubtc.c
+++ ubtc.c
@@ -11,12 +11,26 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define PATH_SIZE 4096
+
+#define TRACE(fmt, ...) do { \
+    fprintf(stderr, "%s:%u " fmt "\n", __FILE__, __LINE__, __VA_ARGS__); \
+} while (0)
+
+struct Client {
+    FILE* in;
+    FILE* out;
+    char root[PATH_SIZE];
+};
+
+typedef struct Client Client;
+
 static int
-recv_changed()
+recv_changed(Client* client)
 {
     size_t size = 4096;
     char buf[size];
-    if (fgets(buf, size, stdin) == NULL) {
+    if (fgets(buf, size, client->in) == NULL) {
         perror("fgets failed");
         abort();
     }
@@ -25,11 +39,11 @@ recv_changed()
 }
 
 static void
-recv_ok()
+recv_ok(Client* client)
 {
     size_t size = 4096;
     char buf[size];
-    if (fgets(buf, size, stdin) == NULL) {
+    if (fgets(buf, size, client->in) == NULL) {
         perror("fgets failed");
         abort();
     }
@@ -42,22 +56,26 @@ recv_ok()
 }
 
 static void
-send(const char* fmt, ...)
+send(Client* client, const char* fmt, ...)
 {
+    FILE* fp = client->out;
     va_list ap;
     va_start(ap, fmt);
-    vprintf(fmt, ap);
+    vfprintf(fp, fmt, ap);
     va_end(ap);
-    printf("\r\n");
-    fflush(stdout);
+    fprintf(fp, "\r\n");
+    fflush(fp);
 }
 
 static void
 quote(char* dest, const char* path)
 {
+    char* q = dest;
+    *q = '\"';
+    q++;
+
     const char* p;
-    char* q;
-    for (p = path, q = dest; *p != '\0'; p++) {
+    for (p = path; *p != '\0'; p++) {
         char c = *p;
         if ((c == '\"') || (c == '\\')) {
             *q = '\\';
@@ -66,6 +84,7 @@ quote(char* dest, const char* path)
         *q = c;
         q++;
     }
+    strcpy(q, "\"");
 }
 
 static void
@@ -75,7 +94,7 @@ get_path_from_root(char* dest, const char* root, const char* path)
 }
 
 static void
-send_dir(const char* root, const char* path)
+send_dir(Client* client, const char* path)
 {
     struct stat sb;
     if (lstat(path, &sb) != 0) {
@@ -83,36 +102,40 @@ send_dir(const char* root, const char* path)
         return;
     }
     char path_from_root[strlen(path) + 1];
-    get_path_from_root(path_from_root, root, path);
+    get_path_from_root(path_from_root, client->root, path);
     char buf[2 * strlen(path_from_root) + 3];
     quote(buf, path_from_root);
-    send("DIR %s %o %d %d", buf, 0777 & sb.st_mode, sb.st_uid, sb.st_gid);
-    recv_ok();
+    const char* fmt = "DIR %s %o %d %d";
+    send(client, fmt, buf, 0777 & sb.st_mode, sb.st_uid, sb.st_gid);
+    recv_ok(client);
 }
 
+#define array_sizeof(a) (sizeof(a) / sizeof(a[0]))
+
 static void
-backup_parent(const char* root, const char* path)
+backup_parent(Client* client, const char* path)
 {
+    if (strcmp(client->root, path) == 0) {
+        return;
+    }
+
     const char* parent = dirname(path);
     if (parent == NULL) {
         perror("dirname failed");
         return;
     }
-    if (strcmp(parent, root) == 0) {
-        return;
-    }
     char dir[strlen(parent) + 1];
     strcpy(dir, parent);
-    backup_parent(root, dir);
+    backup_parent(client, dir);
 
-    send_dir(root, dir);
+    send_dir(client, dir);
 }
 
 static void
-send_symlink(const char* root, const char* path)
+send_symlink(Client* client, const char* path)
 {
     char path_from_root[strlen(path) + 1];
-    get_path_from_root(path_from_root, root, path);
+    get_path_from_root(path_from_root, client->root, path);
     char quoted_path[2 * strlen(path_from_root) + 3];
     quote(quoted_path, path_from_root);
 
@@ -135,15 +158,15 @@ send_symlink(const char* root, const char* path)
 
     const char* fmt = "SYMLINK %s %o %u %u %s";
     mode_t mode = 0777 & sb.st_mode;
-    send(fmt, quoted_path, mode, sb.st_uid, sb.st_gid, quoted_src);
-    recv_ok();
+    send(client, fmt, quoted_path, mode, sb.st_uid, sb.st_gid, quoted_src);
+    recv_ok(client);
 }
 
 static void
-send_locked_file(const char* root, const char* path, FILE* fp)
+send_locked_file(Client* client, const char* path, FILE* fp)
 {
     char path_from_root[strlen(path) + 1];
-    get_path_from_root(path_from_root, root, path);
+    get_path_from_root(path_from_root, client->root, path);
     char buf[2 * strlen(path_from_root) + 3];
     quote(buf, path_from_root);
 
@@ -160,28 +183,29 @@ send_locked_file(const char* root, const char* path, FILE* fp)
     strftime(mtime, maxsize, "%Y-%m-%dT%H:%M:%S", &tm);
 
     const char* fmt = "FILE %s %o %u %u %s";
-    send(fmt, buf, 0777 & sb.st_mode, sb.st_uid, sb.st_gid, mtime);
+    send(client, fmt, buf, 0777 & sb.st_mode, sb.st_uid, sb.st_gid, mtime);
 
-    if (recv_changed() != 0) {
+    if (recv_changed(client) != 0) {
         return;
     }
 
     size_t size = sb.st_size;
-    send("BODY %zu", size);
+    send(client, "BODY %zu", size);
     size_t rest = size;
     while (0 < rest) {
         size_t size = 4096;
         char buf[size];
         size_t n = size < rest ? size : rest;
         size_t nbytes = fread(buf, 1, n, fp);
-        fwrite(buf, 1, nbytes, stdout);
+        fwrite(buf, 1, nbytes, client->out);
+        fflush(client->out);
         rest -= nbytes;
     }
-    recv_ok();
+    recv_ok(client);
 }
 
 static void
-send_file(const char* root, const char* path)
+send_file(Client* client, const char* path)
 {
     FILE* fp = fopen(path, "r");
     if (fp == NULL) {
@@ -194,17 +218,17 @@ send_file(const char* root, const char* path)
         fclose(fp);
         return;
     }
-    send_locked_file(root, path, fp);
+    send_locked_file(client, path, fp);
     if (flock(fd, LOCK_UN) != 0) {
         perror("flock failed");
     }
     fclose(fp);
 }
 
-static void backup_dir(const char*, const char*);
+static void backup_dir(Client*, const char*);
 
 static void
-send_dir_entry(const char* root, const char* path, const char* name)
+send_dir_entry(Client* client, const char* path, const char* name)
 {
     if ((strcmp(name, ".") == 0) || (strcmp(name, "..") == 0)) {
         return;
@@ -217,21 +241,20 @@ send_dir_entry(const char* root, const char* path, const char* name)
         return;
     }
     if (S_ISDIR(sb.st_mode)) {
-        backup_dir(root, fullpath);
+        send_dir(client, fullpath);
+        backup_dir(client, fullpath);
         return;
     }
     if (S_ISLNK(sb.st_mode)) {
-        send_symlink(root, fullpath);
+        send_symlink(client, fullpath);
         return;
     }
-    send_file(root, fullpath);
+    send_file(client, fullpath);
 }
 
 static void
-backup_dir(const char* root, const char* path)
+backup_dir(Client* client, const char* path)
 {
-    send_dir(root, path);
-
     DIR* dirp = opendir(path);
     if (dirp == NULL) {
         perror(path);
@@ -239,16 +262,16 @@ backup_dir(const char* root, const char* path)
     }
     struct dirent* e;
     while ((e = readdir(dirp)) != NULL) {
-        send_dir_entry(root, path, e->d_name);
+        send_dir_entry(client, path, e->d_name);
     }
     closedir(dirp);
 }
 
 static void
-backup_tree(const char* root, const char* path)
+backup_tree(Client* client, const char* path)
 {
-    backup_parent(root, path);
-    backup_dir(root, path);
+    backup_parent(client, path);
+    backup_dir(client, path);
 }
 
 static void
@@ -272,8 +295,19 @@ dup_fd(int old, int new)
     }
 }
 
+static FILE*
+do_fdopen(int fd, const char* mode)
+{
+    FILE* fp = fdopen(fd, mode);
+    if (fp == NULL) {
+        perror("fdopen failed");
+        abort();
+    }
+    return fp;
+}
+
 static void
-exec_server(const char* cmd)
+exec_server(Client* client, const char* cmd)
 {
     int c2s[2];
     create_pipe(c2s);
@@ -286,19 +320,21 @@ exec_server(const char* cmd)
         abort();
     }
     if (0 < pid) {
-        close(c2s[READ]);
         close(s2c[WRITE]);
-        return;
-    }
-    close(c2s[WRITE]);
-    close(s2c[READ]);
-    const char* sh = "/bin/sh";
-    if (execl(sh, sh, "-c", cmd, NULL) == -1) {
-        perror("execv failed");
+        close(c2s[READ]);
+        client->in = do_fdopen(s2c[READ], "r");
+        client->out = do_fdopen(c2s[WRITE], "w");
         return;
     }
     dup_fd(c2s[READ], 0);
     dup_fd(s2c[WRITE], 1);
+    close(c2s[WRITE]);
+    close(s2c[READ]);
+    const char* sh = "/bin/sh";
+    if (execl(sh, sh, "-c", cmd, NULL) == -1) {
+        perror("execl failed");
+    }
+    /* NOTREACHED */
 }
 
 static void
@@ -306,9 +342,6 @@ usage(const char* ident)
 {
     printf("%s [--command=cmd] [--root=root] dir ...\n", ident);
 }
-
-#define array_sizeof(a) (sizeof(a) / sizeof(a[0]))
-#define PATH_SIZE 4096
 
 static void
 absolutize_path(char* dest, size_t size, const char* path)
@@ -338,8 +371,13 @@ get_next_path_element(char* dest, size_t size, const char* path)
         strncpy(dest, path, size);
         return;
     }
-    size_t name_size = p - path + 1;
-    strncpy(dest, path, name_size < size ? name_size : size);
+    size_t name_size = p - path;
+    if (size < name_size + 1) {
+        fprintf(stderr, "Name too long: %s", path);
+        abort();
+    }
+    memcpy(dest, path, name_size);
+    dest[name_size] = '\0';
 }
 
 static void
@@ -353,30 +391,55 @@ remove_parent(char** p, const char* top)
 }
 
 static void
+remove_trailing_path_separators(char* p, const char* limit)
+{
+    char* q = p - 1;
+    while ((limit < q) && (*q == '/')) {
+        *q = '\0';
+        q--;
+    }
+}
+
+static void
 normalize_path(char* dest, size_t size, const char* path)
 {
     char abs_path[PATH_SIZE];
     absolutize_path(abs_path, array_sizeof(abs_path), path);
 
+    char* end = dest + size;
     const char* from = abs_path;
     char* to = dest;
     while (*from != '\0') {
         if (*from == '/') {
             *to = *from;
             skip_path_separators(&from);
+            to++;
             continue;
         }
         char name[PATH_SIZE];
         get_next_path_element(name, array_sizeof(name), from);
         if (strcmp(name, ".") == 0) {
-            /* Do nothing */
+            from += strlen(name);
+            skip_path_separators(&from);
+            continue;
         }
         if (strcmp(name, "..") == 0) {
             remove_parent(&to, path);
+            from += strlen(name);
+            skip_path_separators(&from);
+            continue;
         }
-        from += strlen(name);
+        size_t len = strlen(name);
+        if (end <= to + len) {
+            fprintf(stderr, "Path too long: %s", path);
+            abort();
+        }
+        strcpy(to, name);
+        from += len;
+        to += len;
     }
     *to = *from;
+    remove_trailing_path_separators(to, path);
 }
 
 int
@@ -391,7 +454,7 @@ main(int argc, char* argv[])
     const char* cmd = "ssh windsor ~/projects/UnnamedBackupTool/ubts";
     const char* root = "/";
     int opt;
-    while ((opt = getopt_long_only(argc, argv, NULL, opts, NULL)) != -1) {
+    while ((opt = getopt_long_only(argc, argv, "", opts, NULL)) != -1) {
         switch (opt) {
         case 'c':
             cmd = optarg;
@@ -405,16 +468,16 @@ main(int argc, char* argv[])
         }
     }
 
-    char abs_root[PATH_SIZE];
-    normalize_path(abs_root, array_sizeof(abs_root), root);
+    Client client;
+    normalize_path(client.root, PATH_SIZE, root);
 
-    exec_server(cmd);
+    exec_server(&client, cmd);
 
     int i;
     for (i = optind; i < argc; i++) {
         char abs_path[PATH_SIZE];
         normalize_path(abs_path, array_sizeof(abs_path), argv[i]);
-        backup_tree(abs_root, abs_path);
+        backup_tree(&client, abs_path);
     }
     return 0;
 }
